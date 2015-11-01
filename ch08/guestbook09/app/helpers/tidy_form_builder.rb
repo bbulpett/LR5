7c3f89f4f4f57b95cd80780da1e9b12c354b5b52
Barnabas Bulpett <bbulpett@charter.net>
2015-11-01 09:19:33 -0500

    TidyFormBuilder label generation methods

--- ch08/guestbook09/app/helpers/tidy_form_builder.rb
+++ ch08/guestbook09/app/helpers/tidy_form_builder.rb
@@ -19,4 +19,54 @@ class TidyFormBuilder < ActionView::Helpers::FormBuilder
                      ['United States of America', 'USA']],
                   options, html_options)
   end
+
+  # To change the way Rails normally behaves, we need a 
+   # little knowledge about how the form helpers are usually called.
+   # We can get this from the Rails API documentation.
+   
+   # Most of the helpers have a similar signature
+   
+  def text_field(method, options={})
+    # Helpers just return text to be included in the output
+    # Here we create our label, and add it onto the front
+    # of the superclass (ie. the default helper) output
+    label = label_for(method, options) + super(method, options)
+  end
+
+  def text_area(method, options={})
+     label_for(method, options) + super(method, options)
+  end
+ 
+  def password_field(method, options={})
+    label_for(method, options) + super(method, options)
+  end
+   
+  def file_field(method, options={})
+    label_for(method, options) + super(method, options)
+  end
+
+  # Other helpers are slightly more complex
+
+  def date_select(method, options = {}, html_options = {})
+    label_for(method, options) + super(method, options, html_options)
+  end
+
+  def select(method, choices, options = {}, html_options = {})
+     label_for(method, options) + super(method, choices, options, html_options)
+  end
+
+  def time_select(method, options = {}, html_options = {})
+    label_for(method, options) + super(method, options, html_options)
+  end
+
+  def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
+    label_for(method, options) + super(method, options, checked_value, unchecked_value)
+  end
+
+  private
+
+  def label_for(method, options={})
+    (label( options.delete(:label)|| method).safe_concat("<br />"))
+  end
+
 end
