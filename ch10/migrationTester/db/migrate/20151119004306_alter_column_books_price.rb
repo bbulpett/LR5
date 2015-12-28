86051ede3a45424fd04454749845d5ca67556c8b
Barnabas Bulpett <bbulpett@charter.net>
2015-12-27 23:33:02 -0500

    Rewrite change_column migration so it is reversible

--- ch10/migrationTester/db/migrate/20151119004306_alter_column_books_price.rb
+++ ch10/migrationTester/db/migrate/20151119004306_alter_column_books_price.rb
@@ -1,5 +1,14 @@
 class AlterColumnBooksPrice < ActiveRecord::Migration
-  def change
-  	change_column :books, :price, precision: 7, scale: 2
+  # def change
+  # 	change_column :books, :price, precision: 7, scale: 2
+  # end
+
+  # change_column is not reversible. We should use self.up and self.down instead.
+  def self.up
+    change_column :books, :price, precision: 7, scale: 2  #Change the price column to accept 7 digits
+  end
+  
+  def self.down
+    change_column :books, :price, precision: 6, scale: 2  #Revert the price column back to 6 digits
   end
 end
