e4920cc125a03cda6715315e0bc240fe040ce184
Barnabas Bulpett <bbulpett@charter.net>
2016-01-01 17:18:31 -0500

    Begin model testing, update README.md

--- ch12/students06/test/models/award_test.rb
+++ ch12/students06/test/models/award_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
-class AwardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class Award < ActiveRecord::Base
+  # every award is linked to a student, through student_id
+  belongs_to :student
+
+  validates_presence_of :name, :year
+
+  # particular award can only be given once in every year
+  validates_uniqueness_of :name, :scope => :year,
+    :message => "already been given for that year"
+
+  # we started the award scheme in 1980
+  validates_inclusion_of :year, :in => (1980 .. Date.today.year)
 end
