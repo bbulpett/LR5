811ac74918295c203e7538bf2daaa103e466fc64
Barnabas Bulpett <bbulpett@charter.net>
2016-01-03 19:24:43 -0500

    Completed model testing section and corresponding markdown

--- ch12/students06/test/models/course_test.rb
+++ ch12/students06/test/models/course_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_validity
+    course = Course.new
+    assert !course.valid?
+    course.name = "New course"
+    assert course.valid?
+  end
 end
--- ch12/students06/test/models/student_test.rb
+++ ch12/students06/test/models/student_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  fixtures :students, :courses
+
+  def test_validity
+    jonathan = Student.new({:given_name => "Jonathan",
+        :family_name => "Higgins"})
+    assert !jonathan.valid?, "Should require date of birth, start date"
+    jonathan.date_of_birth = "1989-02-03"
+    jonathan.start_date = Date.today
+    assert jonathan.valid?, "Failed even with all required info"
+  end
+
+
+  def test_name
+    jonathan = Student.new({:given_name => "Jonathan",
+        :family_name => "Higgins"})
+    assert_equal jonathan.name, "Jonathan Higgins", "name method screwed up"
+  end
+
+  def test_enrolled_in
+    tc = students(:tc)
+    assert tc.enrolled_in?(courses(:aviation)), "TC not enrolled in surveillance?"
+    assert !tc.enrolled_in?(courses(:security)), "TC should stay out of security"
+  end
+
+  def test_unenrolled_courses
+    magnum = students(:magnum)
+    rick = students(:rick)
+    assert_equal [courses(:aviation)], magnum.unenrolled_courses
+    assert_equal [courses(:aviation)], rick.unenrolled_courses
+    jonathan = Student.new({:given_name => "Jonathan", :family_name => "Higgins"})
+    assert_equal Course.all, jonathan.unenrolled_courses
+  end
 end
