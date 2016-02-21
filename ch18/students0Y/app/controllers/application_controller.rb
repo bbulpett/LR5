918be6fb00a765b35c663f48d168bfdbc632e895
Barnabas Bulpett <bbulpett@charter.net>
2016-02-20 22:43:33 -0500

    Initial commit students0Y

---
+++ ch18/students0Y/app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+  	redirect_to login_url, alert: "Not Authorized" if current_user.nil?
+  end
+
+  def is_admin
+    redirect_to students_path, alert: "Not authorized" if current_user.nil? or !current_user.admin?
+  end
+end
---
+++ ch18/students0Y/db/schema.rb
@@ -0,0 +1,57 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151201003950) do
+
+  create_table "awards", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "year"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id",  null: false
+    t.integer "student_id", null: false
+  end
+
+  add_index "courses_students", ["course_id", "student_id"], name: "index_courses_students_on_course_id_and_student_id"
+  add_index "courses_students", ["student_id", "course_id"], name: "index_courses_students_on_student_id_and_course_id"
+
+  create_table "students", force: :cascade do |t|
+    t.string   "given_name"
+    t.string   "middle_name"
+    t.string   "family_name"
+    t.date     "date_of_birth"
+    t.decimal  "grade_point_average"
+    t.date     "start_date"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
+  end
+
+end
